fce57718bb41b41118d30a74fa23aae94eee39e0
Cengiz Cebeci <thejengo0o@gmail.com>
2019-03-15 18:46:16 +0300

    TransactionsService added and Served

--- BankApp-WebService/TransactionWebService.asmx.cs
+++ BankApp-WebService/TransactionWebService.asmx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Services;
 using Bank.BusinessLogic;
+using Bank.Commons.Concretes.Helpers;
+using Bank.Commons.Concretes.Logger;
 using Bank.Models.Concretes;
 
 namespace BankApp_WebService
@@ -20,9 +22,90 @@ namespace BankApp_WebService
     {
 
         [WebMethod]
-        public string HelloWorld()
+        public bool Deposit(int senderId, decimal amount)
         {
-            return "Hello World";
+            try
+            {
+                using (var transactionBussiness = new TransactionBusiness())
+                {
+                    Transactions transaction = new Transactions()
+                    {
+                        TransactorAccountNumber = senderId,
+                        ReceiverAccountNumber = senderId,
+                        TransactionAmount = amount,
+                        TransactionDate = DateTime.Now,
+                        isSuccess = false
+                    };
+
+                    using (var customerBussiness = new CustomersBusiness())
+                    {
+                        return transactionBussiness.DepositMoney(transaction, customerBussiness.SelectCustomerById(senderId));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogHelper.Log(LogTarget.File,
+                    "Deposit failed: " + senderId + "." + "\n" + ExceptionHelper.ExceptionToString(e));
+                return false;
+            }
+        }
+
+        [WebMethod]
+        public bool Withdraw(int senderId, decimal amount)
+        {
+            try
+            {
+                using (var transactionBussiness = new TransactionBusiness())
+                {
+                    Transactions transaction = new Transactions()
+                    {
+                        TransactorAccountNumber = senderId,
+                        ReceiverAccountNumber = null,
+                        TransactionAmount = amount,
+                        TransactionDate = DateTime.Now,
+                        isSuccess = false
+                    };
+
+                    using (var customerBussiness = new CustomersBusiness())
+                    {
+                        return transactionBussiness.WithdrawMoney(transaction, customerBussiness.SelectCustomerById(senderId));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogHelper.Log(LogTarget.File, "Withdraw failed: " + senderId + "." + "\n" + ExceptionHelper.ExceptionToString(e));
+                return false;
+            }
+        }
+
+        [WebMethod]
+        public bool Transfer(int senderId, int receiverId, decimal amount)
+        {
+            try
+            {
+                using (var transactionBussiness = new TransactionBusiness())
+                {
+                    Transactions transaction = new Transactions()
+                    {
+                        TransactorAccountNumber = senderId,
+                        ReceiverAccountNumber = receiverId,
+                        TransactionAmount = amount,
+                        TransactionDate = DateTime.Now,
+                        isSuccess = false
+                    };
+                    using (var customerBussiness = new CustomersBusiness())
+                    {
+                        return transactionBussiness.MakeTransaction(transaction, customerBussiness.SelectCustomerById(senderId),customerBussiness.SelectCustomerById(receiverId));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogHelper.Log(LogTarget.File,"Transfer failed betweeen: "+ senderId + " and "+ receiverId + "." + "\n" + ExceptionHelper.ExceptionToString(e));
+                return false;
+            }
         }
     }
 }
